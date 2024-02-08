5a037d056e949ee5b37f1be06ffe44f7f8678c48
Cleberson <pauluci.cleberson@gmail.com>
2024-02-08 14:47:21 -0300

    Using util getters to get icon and formatting the date

--- lib/widgets/expenses_list/expense_item.dart
+++ lib/widgets/expenses_list/expense_item.dart
@@ -22,9 +22,9 @@ class ExpenseItem extends StatelessWidget {
                 const Spacer(),
                 Row(
                   children: [
-                    const Icon(Icons.calendar_month),
+                    Icon(expense.categoryIconData),
                     const SizedBox(width: 8),
-                    Text(expense.date.toString()),
+                    Text(expense.formattedDate),
                   ],
                 ),
               ],
