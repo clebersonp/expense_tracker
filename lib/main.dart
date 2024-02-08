47e64939edd35435dd25635c1482314dfcfa634d
Cleberson <pauluci.cleberson@gmail.com>
2024-02-08 10:24:40 -0300

    Started to write expenses widget code

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_expense_tracker/expenses.dart';
 
 void main() {
-  runApp(const MaterialApp());
+  runApp(
+    const MaterialApp(
+      home: Expenses(),
+    ),
+  );
 }
